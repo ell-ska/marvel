8b7acfabdf554f4a23a265e1ecf28fda2d1e8da9
ell-ska <ella@skagerstrom.se>
2023-01-10 11:01:03 +0100

    add new pages and links in menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 $(function() {
     $('.header__menu-button').on('click', () => {
+        $('.container').toggleClass('container--locked')
         $('.header__menu-button').toggleClass('header__menu-button--clicked')
         $('.nav').toggleClass('display-none')
     })
